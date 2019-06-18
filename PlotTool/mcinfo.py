4337794f2732b380c81284625bc80a33eae4db14
ekoenig4 <ekoenig4@wisc.edu>
2019-06-18 08:05:21 -0500

    Updated 2018 xs, added update.py to handle local version control

--- PlotTool/mcinfo.py
+++ PlotTool/mcinfo.py
@@ -12,7 +12,7 @@ ZJets_FileNames = ["postZ100to200","postZ200to400","postZ400to600","postZ600to80
 
 GJets_FileNames = ["postGJets40to100","postGJets100to200","postGJets200to400","postGJets400to600","postGJets600toInf"];
 
-DYJets_FileNames = ["postDY100to200","postDY200to400","postDY400to600","postDY600to800","postDY800to1200","postDY1200to2500","postDY2500toInf","postDY_MLM"]
+DYJets_FileNames = ["postDY70to100","postDY100to200","postDY200to400","postDY400to600","postDY600to800","postDY800to1200","postDY1200to2500","postDY2500toInf"]
 
 TTJets_FileNames = ["postTTJetsDiLept"];#"postTTJets_MLM
 
@@ -21,59 +21,56 @@ DiBoson_FileNames = ["postWW","postWWto4Q","postWWtoLNuQQ","postWZ","postZZ","po
 QCD_FileNames = ["postQCD100to200","postQCD200to300","postQCD300to500","postQCD500to700","postQCD700to1000","postQCD1000to1500","postQCD1500to2000","postQCD2000toInf"];
 
 xsec = {
-'postZ100to200': 302.8,
-'postZ200to400': 92.59,
-'postZ400to600': 13.18,
-'postZ600to800': 3.257,
-'postZ800to1200': 1.49,
-'postZ1200to2500': 0.3419,
-'postZ2500toInf': 0.005146,
-
-'postWJets_MLM': 52940.0,
-'postW100to200': 1395.0,
-'postW200to400': 407.9,
-'postW400to600': 57.48,
-'postW600to800': 12.87,
-'postW800to1200': 5.366,
-'postW1200to2500': 1.074,
-'postW2500toInf': 0.008001,
-
-'postDY_MLM':4895,
-'postDY70to100': '/DYJetsToLL_M-50_HT-70to100_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM',
-'postDY100to200': 161.1,
-'postDY200to400': 48.66,
-'postDY400to600': 6.968,
-'postDY600to800': 1.743,
-'postDY800to1200': 0.8052,
-'postDY1200to2500': 0.1933,
-'postDY2500toInf': 0.003468,
-
-'postGJets40to100': 18700.0,
-'postGJets100to200': 8640.0,
-'postGJets200to400': 2185.0,
-'postGJets400to600':132.5,
-'postGJets600toInf': 85.31,
-
-'postTTJetsDiLept': 54.23,
-'postTTJetsSingleLeptFromT': 109.6,
-'postTTJets_MLM': 722.8,
-
-'postQCD100to200': 23700000.0,
-'postQCD200to300': 1547000.0,
-'postQCD300to500': 322600.0,
-'postQCD500to700': 29980.0,
-'postQCD700to1000': 6334.0,
-'postQCD1000to1500': 1088.0,
-'postQCD1500to2000': 99.11,
-'postQCD2000toInf': 20.23,
-
-
-'postWW': 75.8,
-'postWWtoLNuQQ': 45.99,
-'postWWto2L2Nu': 11.08,
-'postWWto2L2NuPS': 11.08,
-'postWWto4Q': 47.73,
-
-'postWZ': 27.6,
-'postZZ': 12.14
+"postZ100to200":3.045e+02,
+"postZ200to400":9.185e+01,
+"postZ400to600":1.311e+01,
+"postZ600to800":3.257e+00,
+"postZ800to1200":1.499e+00,
+"postZ1200to2500":3.430e-01,
+"postZ2500toInf":5.146e-03,
+    
+"postWJets_MLM":5.279e+04,
+"postW100to200":1.395e+03,
+"postW200to400":4.093e+02,
+"postW400to600":5.791e+01,
+"postW600to800":1.293e+01,
+"postW800to1200":5.395e+00,
+"postW1200to2500":1.080e+00,
+"postW2500toInf":8.053e-03,
+    
+"postDY70to100":1.471e+02,
+"postDY100to200":1.610e+02,
+"postDY200to400":4.858e+01,
+"postDY400to600":6.982e+00,
+"postDY600to800":1.747e+00,
+"postDY800to1200":8.052e-01,
+"postDY1200to2500":1.927e-01,
+"postDY2500toInf":3.478e-03,
+    
+"postGJets40to100":1.862e+04,
+"postGJets100to200":8.622e+03,
+"postGJets200to400":2.193e+03,
+"postGJets400to600":2.585e+02,
+"postGJets600toInf":8.521e+01,
+    
+"postTTJetsDiLept":5.424e+01,
+"postTTJetsSingleLeptFromT":1.092e+02,
+"postTTJets_MLM":7.268e+02,
+    
+"postQCD100to200":2.369e+07,
+"postQCD200to300":1.556e+06,
+"postQCD300to500":3.236e+05,
+"postQCD500to700":2.999e+04,
+"postQCD700to1000":6.351e+03,
+"postQCD1000to1500":1.094e+03,
+"postQCD1500to2000":9.901e+01,
+"postQCD2000toInf":2.023e+01,
+    
+"postWW":7.590e+01,
+"postWWtoLNuQQ":4.599e+01,
+"postWWto2L2Nu":1.108e+01,
+"postWWto4Q":4.773e+01,
+    
+"postWZ":2.757e+01,
+"postZZ":1.214e+01
 }
