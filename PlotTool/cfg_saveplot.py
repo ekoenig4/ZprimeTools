67a1fc30d58662775d78d4ae90147a9b6d32bb33
ekoenig4 <ekoenig4@wisc.edu>
2019-09-04 10:58:35 -0500

    added transfer factor plots to saveplot

--- PlotTool/cfg_saveplot.py
+++ PlotTool/cfg_saveplot.py
@@ -3,60 +3,64 @@ config = {
     'regions':{'SignalRegion/':'9','DoubleEleCR/':'11','DoubleMuCR/':'11','SingleEleCR/':'11','SingleMuCR/':'11'},
     'Uncertainty':{
         'psw': {
-            dir:'PS_Weights/',
             'region':[],
             'unc':{
-                'isrRedUp':{id:'2', eval:'muRfac=0.707'},'fsrRedUp':{id:'3', eval:'muRfac=0.707'}, 'isrRedDown':{id:'4', eval:'muRfac=1.414'},'fsrRedDown':{id:'5', eval:'muRfac=1.414' },
-                'isrDefUp':{id:'6', eval:'muRfac=0.5'  },'fsrDefUp':{id:'7', eval:'muRfac=0.5'  }, 'isrDefDown':{id:'8', eval:'muRfac=2.0'  },'fsrDefDown':{id:'9', eval:'muRfac=2.0'   },
-                'isrConUp':{id:'10', eval:'muRfac=0.25' },'fsrConUp':{id:'11', eval:'muRfac=0.25' }, 'isrConDown':{id:'12', eval:'muRfac=4.0'  },'fsrConDown':{id:'13', eval:'uRfac=4.0'  },
-                # 'fsr_G2GG_muRDown':{id:'14',eval:'G2GG:muRfac=0.5'},
-                # 'fsr_G2GG_muRUp':  {id:'15',eval:'G2GG:muRfac=2.0'},
-                # 'fsr_G2QQ_muRDown':{id:'16',eval:'G2QQ:muRfac=0.5'},
-                # 'fsr_G2QQ_muRUp':  {id:'17',eval:'G2QQ:muRfac=2.0'},
-                # 'fsr_Q2QG_muRDown':{id:'18',eval:'Q2QG:muRfac=0.5'},
-                # 'fsr_Q2QG_muRUp':  {id:'19',eval:'Q2QG:muRfac=2.0'},
-                # 'fsr_X2XG_muRDown':{id:'20',eval:'X2XG:muRfac=0.5'},
-                # 'fsr_X2XG_muRUp':  {id:'21',eval:'X2XG:muRfac=2.0'},
-                # 'fsr_G2GG_cNSDown':{id:'22',eval:'G2GG:cNS=-2.0'  },
-                # 'fsr_G2GG_cNSUp':  {id:'23',eval:'G2GG:cNS=2.0'   },
-                # 'fsr_G2QQ_cNSDown':{id:'24',eval:'G2QQ:cNS=-2.0'  },
-                # 'fsr_G2QQ_cNSUp':  {id:'25',eval:'G2QQ:cNS=2.0'   },
-                # 'fsr_Q2QG_cNSDown':{id:'26',eval:'Q2QG:cNS=-2.0'  },
-                # 'fsr_Q2QG_cNSUp':  {id:'27',eval:'Q2QG:cNS=2.0'   },
-                # 'fsr_X2XG_cNSDown':{id:'28',eval:'X2XG:cNS=-2.0'  },
-                # 'fsr_X2XG_cNSUp':  {id:'29',eval:'X2XG:cNS=2.0'   },
-                # 'isr_G2GG_muRDown':{id:'30',eval:'G2GG:muRfac=0.5'},
-                # 'isr_G2GG_muRUp':  {id:'31',eval:'G2GG:muRfac=2.0'},
-                # 'isr_G2QQ_muRDown':{id:'32',eval:'G2QQ:muRfac=0.5'},
-                # 'isr_G2QQ_muRUp':  {id:'33',eval:'G2QQ:muRfac=2.0'},
-                # 'isr_Q2QG_muRDown':{id:'34',eval:'Q2QG:muRfac=0.5'},
-                # 'isr_Q2QG_muRUp':  {id:'35',eval:'Q2QG:muRfac=2.0'},
-                # 'isr_X2XG_muRDown':{id:'36',eval:'X2XG:muRfac=0.5'},
-                # 'isr_X2XG_muRUp':  {id:'37',eval:'X2XG:muRfac=2.0'},
-                # 'isr_G2GG_cNSDown':{id:'38',eval:'G2GG:cNS=-2.0'  },
-                # 'isr_G2GG_cNSUp':  {id:'39',eval:'G2GG:cNS=2.0'   },
-                # 'isr_G2QQ_cNSDown':{id:'40',eval:'G2QQ:cNS=-2.0'  },
-                # 'isr_G2QQ_cNSUp':  {id:'41',eval:'G2QQ:cNS=2.0'   },
-                # 'isr_Q2QG_cNSDown':{id:'42',eval:'Q2QG:cNS=-2.0'  },
-                # 'isr_Q2QG_cNSUp':  {id:'43',eval:'Q2QG:cNS=2.0'   },
-                # 'isr_X2XG_cNSDown':{id:'44',eval:'X2XG:cNS=-2.0'  },
-                # 'isr_X2XG_cNSUp':  {id:'45',eval:'X2XG:cNS=2.0'   }
+                'isrRedUp':{id:'1', eval:'muRfac=0.707'},'fsrRedUp':{id:'2', eval:'muRfac=0.707'}, 'isrRedDown':{id:'3', eval:'muRfac=1.414'},'fsrRedDown':{id:'4', eval:'muRfac=1.414' },
+                'isrDefUp':{id:'5', eval:'muRfac=0.5'  },'fsrDefUp':{id:'6', eval:'muRfac=0.5'  }, 'isrDefDown':{id:'7', eval:'muRfac=2.0'  },'fsrDefDown':{id:'8', eval:'muRfac=2.0'   },
+                'isrConUp':{id:'9', eval:'muRfac=0.25' },'fsrConUp':{id:'10', eval:'muRfac=0.25' }, 'isrConDown':{id:'11', eval:'muRfac=4.0'  },'fsrConDown':{id:'12', eval:'uRfac=4.0'  },
+                # 'fsr_G2GG_muRDown':{id:'13',eval:'G2GG:muRfac=0.5'},
+                # 'fsr_G2GG_muRUp':  {id:'14',eval:'G2GG:muRfac=2.0'},
+                # 'fsr_G2QQ_muRDown':{id:'15',eval:'G2QQ:muRfac=0.5'},
+                # 'fsr_G2QQ_muRUp':  {id:'16',eval:'G2QQ:muRfac=2.0'},
+                # 'fsr_Q2QG_muRDown':{id:'17',eval:'Q2QG:muRfac=0.5'},
+                # 'fsr_Q2QG_muRUp':  {id:'18',eval:'Q2QG:muRfac=2.0'},
+                # 'fsr_X2XG_muRDown':{id:'19',eval:'X2XG:muRfac=0.5'},
+                # 'fsr_X2XG_muRUp':  {id:'20',eval:'X2XG:muRfac=2.0'},
+                # 'fsr_G2GG_cNSDown':{id:'21',eval:'G2GG:cNS=-2.0'  },
+                # 'fsr_G2GG_cNSUp':  {id:'22',eval:'G2GG:cNS=2.0'   },
+                # 'fsr_G2QQ_cNSDown':{id:'23',eval:'G2QQ:cNS=-2.0'  },
+                # 'fsr_G2QQ_cNSUp':  {id:'24',eval:'G2QQ:cNS=2.0'   },
+                # 'fsr_Q2QG_cNSDown':{id:'25',eval:'Q2QG:cNS=-2.0'  },
+                # 'fsr_Q2QG_cNSUp':  {id:'26',eval:'Q2QG:cNS=2.0'   },
+                # 'fsr_X2XG_cNSDown':{id:'27',eval:'X2XG:cNS=-2.0'  },
+                # 'fsr_X2XG_cNSUp':  {id:'28',eval:'X2XG:cNS=2.0'   },
+                # 'isr_G2GG_muRDown':{id:'29',eval:'G2GG:muRfac=0.5'},
+                # 'isr_G2GG_muRUp':  {id:'30',eval:'G2GG:muRfac=2.0'},
+                # 'isr_G2QQ_muRDown':{id:'31',eval:'G2QQ:muRfac=0.5'},
+                # 'isr_G2QQ_muRUp':  {id:'32',eval:'G2QQ:muRfac=2.0'},
+                # 'isr_Q2QG_muRDown':{id:'33',eval:'Q2QG:muRfac=0.5'},
+                # 'isr_Q2QG_muRUp':  {id:'34',eval:'Q2QG:muRfac=2.0'},
+                # 'isr_X2XG_muRDown':{id:'35',eval:'X2XG:muRfac=0.5'},
+                # 'isr_X2XG_muRUp':  {id:'36',eval:'X2XG:muRfac=2.0'},
+                # 'isr_G2GG_cNSDown':{id:'37',eval:'G2GG:cNS=-2.0'  },
+                # 'isr_G2GG_cNSUp':  {id:'38',eval:'G2GG:cNS=2.0'   },
+                # 'isr_G2QQ_cNSDown':{id:'39',eval:'G2QQ:cNS=-2.0'  },
+                # 'isr_G2QQ_cNSUp':  {id:'40',eval:'G2QQ:cNS=2.0'   },
+                # 'isr_Q2QG_cNSDown':{id:'41',eval:'Q2QG:cNS=-2.0'  },
+                # 'isr_Q2QG_cNSUp':  {id:'42',eval:'Q2QG:cNS=2.0'   },
+                # 'isr_X2XG_cNSDown':{id:'43',eval:'X2XG:cNS=-2.0'  },
+                # 'isr_X2XG_cNSUp':  {id:'44',eval:'X2XG:cNS=2.0'   }
             }
         },
         'pfu': {
-            dir:'PFUncertainty/',
             'region':[],
             'unc':{
-                'trackerUp'  :{id:'46'},'ecalUp'  :{id:'47'},'hcalUp'  :{id:'48'},
-                'trackerDown':{id:'49'},'ecalDown':{id:'50'},'hcalDown':{id:'51'}
+                'trackerUp'  :{id:'45'},'ecalUp'  :{id:'46'},'hcalUp'  :{id:'47'},
+                'trackerDown':{id:'48'},'ecalDown':{id:'49'},'hcalDown':{id:'50'}
             }
         },
         'jes': {
-            dir:'JetEnergyScale/',
             'region':[],
             'unc': {
-                'jesUp':  {id:'52'},
-                'jesDown':{id:'53'}
+                'jesUp':  {id:'51'},
+                'jesDown':{id:'52'}
+            }
+        },
+        'ewk':{
+            'region':[],
+            'unc': {
+                'ewkUp':  {id:'53'},
+                'ewkDown':{id:'54'},
             }
         },
     }
--- PlotTool/saveplot.py
+++ PlotTool/saveplot.py
@@ -3,100 +3,116 @@
 from ROOT import *
 from sys import argv
 from sys import path
-import Plot as plot
-from os import system,getcwd
+from Plot import datamc,getargs,GetRatio
+from os import system,getcwd,path,mkdir
 from cfg_saveplot import config
-from mcinfo import lumi as mc
+from mcinfo import lumi as mc,version
 from optparse import OptionParser
 
 gROOT.SetBatch(1)
 
-def greaterThan(hs,cut):
-    if hs == None: return '+'+str(cut)
-    xaxis = [ hs.GetBinLowEdge(i) for i,v in enumerate(hs) ]
-    for i,x in enumerate(xaxis):
-        if x < cut: hs.SetBinContent(i,0); hs.SetBinError(i,0)
-########################################################################################################
-def lessThan(hs,cut):
-    if hs == None: return '+'+str(cut)
-    xaxis = [ hs.GetBinLowEdge(i) for i,v in enumerate(hs) ]
-    for i,x in enumerate(xaxis):
-        if x > cut: hs.SetBinContent(i,0); hs.SetBinError(i,0)
-########################################################################################################
-rfile = TFile('Systematics_'+str(config['year'])+'.root','recreate')
+outdir = "Systematics"
+if not path.isdir(outdir): mkdir(outdir)
 dir = {"SignalRegion/":"sr","DoubleEleCR/":"ee","DoubleMuCR/":"mm","SingleEleCR/":"e","SingleMuCR/":"m"}
 
-cut = None
-if '>' in argv[-1]:
-    cut = ( greaterThan,float(argv[-1].replace('>','')) )
-    argv.pop(-1)
-elif '<' in argv[-1]:
-    cut = ( lessThan,float(argv[-1].replace('<','')) )
-    argv.pop(-1)
-variable = argv[-1]
-samples = {}
-Uncertainty = config['Uncertainty']
-lumi = max( lumi for region,lumi in mc.items() )
-for region,nhisto in config['regions'].items():
-    print region
-    directory = rfile.mkdir(dir[region])
-    # store[dir] = directory
-    norm = plot.datamc(fileDir=region,lumi=lumi,show=False)
-    norm.initiate(variable+'_'+nhisto)
-    directory.cd()
-    sumOfBkg = norm.getSumOfBkg()
-    if cut != None: cut[0](sumOfBkg,cut[1])
-    sumOfBkg.SetName('sumOfBkg')
-    sumOfBkg.Write()
-    for sample in norm.SampleList:
-        if sample == 'Data':
-            data_obs = norm.getSumOfBkg()
-            if cut != None: cut[0](data_obs,cut[1])
-            data_obs.SetName('data_obs')
-            data_obs.Write()
-        elif sample == "Signal":
-            for signal in norm.signal:
-                if cut != None: cut[0](norm.histo[signal],cut[1])
-                norm.histo[signal].SetName(signal)
-                norm.histo[signal].Write()
-        else:
-            if cut != None: cut[0](norm.histo[sample],cut[1])
-            norm.histo[sample].SetName(sample)
-            norm.histo[sample].Write()
-    ##############################################
-    sample_unc = { name:unc for name,unc in Uncertainty.items() if not any(unc['region']) or region in unc['region'] }
-    for name,unc in sample_unc.items():
-        for var,info in unc['unc'].items():
-            if config['year'] != 2016 and region == 'SignalRegion/':
-                info[id] = str( int(info[id])-1 )
-            print '\t',var
-            input = variable+'_'+str( int(info[id]) + int(nhisto) )
-            norm.initiate( input )
-            directory.cd()
-            sumOfBkg = norm.getSumOfBkg()
-            if cut != None: cut[0](sumOfBkg,cut[1])
-            sumOfBkg.SetName('sumOfBkg_'+var)
-            sumOfBkg.Write()
-            for sample in norm.SampleList:
-                if sample == 'Data': continue
-                elif sample == "Signal":
-                    for signal in norm.signal:
-                        if cut != None: cut[0](norm.histo[signal],cut[1])
-                        norm.histo[signal].SetName(signal+'_'+var)
-                        norm.histo[signal].Write()
-                else:
-                    if cut != None: cut[0](norm.histo[sample],cut[1])
-                    norm.histo[sample].SetName(sample+'_'+var)
-                    norm.histo[sample].Write()
-    ###############################################################
+def GetTransferFactors(rfile):
+    if type(rfile) == str: rfile = TFile.Open(rfile)
+    tfactors = {'ZJets':{},'WJets':{}}
+    varmap = {'ZJets':{'sample':'DYJets','regions':['ee','mm']},
+              'WJets':{'sample':'WJets' ,'regions':['e','m']}}
+    for sample,transfer in tfactors.items():
+        rfile.cd(); rfile.cd('sr')
+        sr = { key.GetName():rfile.Get('sr/%s' % key.GetName()).Clone() for key in gDirectory.GetListOfKeys()
+               if sample in key.GetName() }
+        infomap = varmap[sample]
+        for region in infomap['regions']:
+            thistos = {}
+            rfile.cd(); rfile.cd(region)
+            cr = { key.GetName().replace(infomap['sample'],sample):rfile.Get( '%s/%s' % (region,key.GetName()) ).Clone() for key in gDirectory.GetListOfKeys()
+                   if infomap['sample'] in key.GetName() }
+            for key in cr:
+                cr_hs = cr[key]
+                sr_hs = sr[key]
+                thistos[key] = GetRatio(cr_hs,sr_hs).Clone(key)
+            transfer[region] = thistos
+    # Write transfer factor histograms to systematic file
+    for sample,transfer in tfactors.items():
+        for region,histos in transfer.items():
+            rfile.cd(); rfile.cd(region);
+            rfile.mkdir('%s/transfer' % region); rfile.cd('%s/transfer' % region)
+            for key,hs in histos.items():
+                hs.Write()
 ###################################################################
-rfile.cd()
-lumi_hs = TH1F("lumi","lumi",1,0,1)
-lumi_hs.SetBinContent(1,norm.lumi)
-lumi_hs.Write()
-year_hs = TH1F("year","year",1,0,1)
-year_hs.SetBinContent(1,int(norm.version))
-year_hs.Write()
-if cut != None: variable = variable+cut[0](None,cut[1])
-var_hs = TH1F("variable",variable+';'+norm.name,1,0,1)
-var_hs.Write()
+
+def saveplot(variable):
+    print variable
+    cut = ''
+    if '>' in variable: cut = '>'+variable.split('>')[-1]
+    if '<' in variable: cut = '<'+variable.split('<')[-1]
+    rfile = TFile( "%s/%s_%s.sys.root" % (outdir,variable,version) ,'recreate')
+    Uncertainty = config['Uncertainty']
+    lumi = max( lumi for region,lumi in mc.items() )
+    for region,nhisto in config['regions'].items():
+        rfile.cd()
+        var = variable+'_'+nhisto+cut
+        print region
+        directory = rfile.mkdir(dir[region])
+        norm = datamc(fileDir=region,lumi=lumi,show=False)
+        norm.initiate(var)
+        directory.cd()
+        sumOfBkg = norm.getSumOfBkg()
+        sumOfBkg.SetName('sumOfBkg')
+        sumOfBkg.Write()
+        for sample in norm.SampleList:
+            if sample == 'Data':
+                data_obs = norm.getSumOfBkg()
+                data_obs.SetName('data_obs')
+                data_obs.Write()
+            elif sample == "Signal":
+                for signal in norm.signal:
+                    norm.histo[signal].SetName(signal)
+                    norm.histo[signal].Write()
+            else:
+                norm.histo[sample].SetName(sample)
+                norm.histo[sample].Write()
+        ##############################################
+        sample_unc = { name:unc for name,unc in Uncertainty.items() if not any(unc['region']) or region in unc['region'] }
+        for name,unc in sample_unc.items():
+            for var,info in unc['unc'].items():
+                if config['year'] != 2016 and region == 'SignalRegion/':
+                    info[id] = str( int(info[id])-1 )
+                print '\t',var,
+                input = variable+'_'+str( int(info[id]) + int(nhisto) )+cut
+                norm.initiate( input )
+                directory.cd()
+                sumOfBkg = norm.getSumOfBkg()
+                sumOfBkg.SetName('sumOfBkg_'+var)
+                sumOfBkg.Write()
+                for sample in norm.SampleList:
+                    if sample == 'Data': continue
+                    elif sample == "Signal":
+                        for signal in norm.signal:
+                            norm.histo[signal].SetName(signal+'_'+var)
+                            norm.histo[signal].Write()
+                    else:
+                        norm.histo[sample].SetName(sample+'_'+var)
+                        norm.histo[sample].Write()
+                ###############################################################
+            print
+    ###########################################################################
+    GetTransferFactors(rfile)
+    rfile.cd()
+    lumi_hs = TH1F("lumi","lumi",1,0,1)
+    lumi_hs.SetBinContent(1,norm.lumi)
+    lumi_hs.Write()
+    year_hs = TH1F("year","year",1,0,1)
+    year_hs.SetBinContent(1,int(norm.version))
+    year_hs.Write()
+    var_hs = TH1F("variable",variable+';'+norm.name,1,0,1)
+    var_hs.Write()
+    rfile.Close()
+################################################################################
+if __name__ != "__main__": exit()
+
+options,args = getargs()
+for variable in args: saveplot(variable)
