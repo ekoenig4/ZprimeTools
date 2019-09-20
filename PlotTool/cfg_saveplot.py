5fa24645d7d0272ee93c1ec5eb276af96f5ed9e2
ekoenig4 <ekoenig4@wisc.edu>
2019-09-20 13:12:30 -0500

    updated uncertainty format

--- PlotTool/cfg_saveplot.py
+++ PlotTool/cfg_saveplot.py
@@ -2,66 +2,55 @@ config = {
     'year':2018,
     'regions':{'SignalRegion/':'9','DoubleEleCR/':'11','DoubleMuCR/':'11','SingleEleCR/':'11','SingleMuCR/':'11'},
     'Uncertainty':{
-        'psw': {
-            'region':[],
-            'unc':{
-                'isrRedUp':{id:'1', eval:'muRfac=0.707'},'fsrRedUp':{id:'2', eval:'muRfac=0.707'}, 'isrRedDown':{id:'3', eval:'muRfac=1.414'},'fsrRedDown':{id:'4', eval:'muRfac=1.414' },
-                'isrDefUp':{id:'5', eval:'muRfac=0.5'  },'fsrDefUp':{id:'6', eval:'muRfac=0.5'  }, 'isrDefDown':{id:'7', eval:'muRfac=2.0'  },'fsrDefDown':{id:'8', eval:'muRfac=2.0'   },
-                'isrConUp':{id:'9', eval:'muRfac=0.25' },'fsrConUp':{id:'10', eval:'muRfac=0.25' }, 'isrConDown':{id:'11', eval:'muRfac=4.0'  },'fsrConDown':{id:'12', eval:'uRfac=4.0'  },
-                # 'fsr_G2GG_muRDown':{id:'13',eval:'G2GG:muRfac=0.5'},
-                # 'fsr_G2GG_muRUp':  {id:'14',eval:'G2GG:muRfac=2.0'},
-                # 'fsr_G2QQ_muRDown':{id:'15',eval:'G2QQ:muRfac=0.5'},
-                # 'fsr_G2QQ_muRUp':  {id:'16',eval:'G2QQ:muRfac=2.0'},
-                # 'fsr_Q2QG_muRDown':{id:'17',eval:'Q2QG:muRfac=0.5'},
-                # 'fsr_Q2QG_muRUp':  {id:'18',eval:'Q2QG:muRfac=2.0'},
-                # 'fsr_X2XG_muRDown':{id:'19',eval:'X2XG:muRfac=0.5'},
-                # 'fsr_X2XG_muRUp':  {id:'20',eval:'X2XG:muRfac=2.0'},
-                # 'fsr_G2GG_cNSDown':{id:'21',eval:'G2GG:cNS=-2.0'  },
-                # 'fsr_G2GG_cNSUp':  {id:'22',eval:'G2GG:cNS=2.0'   },
-                # 'fsr_G2QQ_cNSDown':{id:'23',eval:'G2QQ:cNS=-2.0'  },
-                # 'fsr_G2QQ_cNSUp':  {id:'24',eval:'G2QQ:cNS=2.0'   },
-                # 'fsr_Q2QG_cNSDown':{id:'25',eval:'Q2QG:cNS=-2.0'  },
-                # 'fsr_Q2QG_cNSUp':  {id:'26',eval:'Q2QG:cNS=2.0'   },
-                # 'fsr_X2XG_cNSDown':{id:'27',eval:'X2XG:cNS=-2.0'  },
-                # 'fsr_X2XG_cNSUp':  {id:'28',eval:'X2XG:cNS=2.0'   },
-                # 'isr_G2GG_muRDown':{id:'29',eval:'G2GG:muRfac=0.5'},
-                # 'isr_G2GG_muRUp':  {id:'30',eval:'G2GG:muRfac=2.0'},
-                # 'isr_G2QQ_muRDown':{id:'31',eval:'G2QQ:muRfac=0.5'},
-                # 'isr_G2QQ_muRUp':  {id:'32',eval:'G2QQ:muRfac=2.0'},
-                # 'isr_Q2QG_muRDown':{id:'33',eval:'Q2QG:muRfac=0.5'},
-                # 'isr_Q2QG_muRUp':  {id:'34',eval:'Q2QG:muRfac=2.0'},
-                # 'isr_X2XG_muRDown':{id:'35',eval:'X2XG:muRfac=0.5'},
-                # 'isr_X2XG_muRUp':  {id:'36',eval:'X2XG:muRfac=2.0'},
-                # 'isr_G2GG_cNSDown':{id:'37',eval:'G2GG:cNS=-2.0'  },
-                # 'isr_G2GG_cNSUp':  {id:'38',eval:'G2GG:cNS=2.0'   },
-                # 'isr_G2QQ_cNSDown':{id:'39',eval:'G2QQ:cNS=-2.0'  },
-                # 'isr_G2QQ_cNSUp':  {id:'40',eval:'G2QQ:cNS=2.0'   },
-                # 'isr_Q2QG_cNSDown':{id:'41',eval:'Q2QG:cNS=-2.0'  },
-                # 'isr_Q2QG_cNSUp':  {id:'42',eval:'Q2QG:cNS=2.0'   },
-                # 'isr_X2XG_cNSDown':{id:'43',eval:'X2XG:cNS=-2.0'  },
-                # 'isr_X2XG_cNSUp':  {id:'44',eval:'X2XG:cNS=2.0'   }
-            }
-        },
-        'pfu': {
-            'region':[],
-            'unc':{
-                'trackerUp'  :{id:'45'},'ecalUp'  :{id:'46'},'hcalUp'  :{id:'47'},
-                'trackerDown':{id:'48'},'ecalDown':{id:'49'},'hcalDown':{id:'50'}
-            }
-        },
-        'jes': {
-            'region':[],
-            'unc': {
-                'jesUp':  {id:'51'},
-                'jesDown':{id:'52'}
-            }
-        },
-        'ewk':{
-            'region':[],
-            'unc': {
-                'ewkUp':  {id:'53'},
-                'ewkDown':{id:'54'},
-            }
-        },
+        'PFU': [ # ID is relative to the regions basic histogram number
+            'PFU_tracker',
+            'PFU_ecal',
+            'PFU_hcal'
+        ],
+        'JES': [
+            'JES'
+        ],
+        'QCD': [
+            "QCD_LO",
+            "QCD_K_LO",
+            "QCD_d1K_LO",
+            "QCD_d2K_LO",
+            "QCD_K_NLO",
+            "QCD_d1K_NLO",
+            "QCD_d2K_NLO",
+            "QCD_d3K_NLO",
+            "QCD_kappa_NLO_EW",
+            "QCD_kappa_NNLO_Sud",
+            "QCD_kappa_EW",
+            "QCD_d1kappa_EW",
+            "QCD_d2kappa_EW",
+            "QCD_d3kappa_EW",
+            "QCD_dK_NLO_mix",
+            "QCD_gammaind_LO"
+        ],
+        'PSW': [
+            # "PSW_isrRed",   
+	    # "PSW_fsrRed",      
+	    # "PSW_isrDef",    
+	    # "PSW_fsrDef",  
+	    "PSW_isrCon",
+	    "PSW_fsrCon",      
+	    # "PSW_fsr_G2GG_muR",
+	    # "PSW_fsr_G2QQ_muR",
+	    # "PSW_fsr_Q2QG_muR",
+	    # "PSW_fsr_X2XG_muR",
+	    # "PSW_fsr_G2GG_cNS",
+	    # "PSW_fsr_G2QQ_cNS",
+	    # "PSW_fsr_Q2QG_cNS",
+	    # "PSW_fsr_X2XG_cNS",
+	    # "PSW_isr_G2GG_muR",
+	    # "PSW_isr_G2QQ_muR",
+	    # "PSW_isr_Q2QG_muR",
+	    # "PSW_isr_X2XG_muR",
+	    # "PSW_isr_G2GG_cNS",
+	    # "PSW_isr_G2QQ_cNS",
+	    # "PSW_isr_Q2QG_cNS",
+	    # "PSW_isr_X2XG_cNS"
+        ]
     }
 }
