be87bd7313e82df227ba28ff995a5a671aa0571a
ekoenig4 <ekoenig4@wisc.edu>
2019-10-08 14:32:44 -0500

    updated sr plot

--- PlotTool/plotShapeUnc.py
+++ PlotTool/plotShapeUnc.py
@@ -119,50 +119,114 @@ def plotCRUnc(sample,uncname):
 def plotSRUnc(sample,uncname):
     print 'Fetching %s' % uncname
     sample.addUnc(uncname)
-    z_norm = sample.processes['ZJets'].histo
-    w_norm = sample.processes['WJets'].histo
+    z_norm = sample.processes['ZJets'].histo.Clone('z_norm')
+    w_norm = sample.processes['WJets'].histo.Clone('w_norm')
 
-    z_up = sample.processes['ZJets'].nuisances[uncname]['Up']
-    z_dn = sample.processes['ZJets'].nuisances[uncname]['Down']
+    z_up = sample.processes['ZJets'].nuisances[uncname]['Up'].Clone('z_up')
+    z_dn = sample.processes['ZJets'].nuisances[uncname]['Down'].Clone('z_dn')
 
-    w_up = sample.processes['WJets'].nuisances[uncname]['Up']
-    w_dn = sample.processes['WJets'].nuisances[uncname]['Down']
+    w_up = sample.processes['WJets'].nuisances[uncname]['Up'].Clone('w_up')
+    w_dn = sample.processes['WJets'].nuisances[uncname]['Down'].Clone('w_dn')
 
-    bins = []
-    for w_h in (w_up,w_dn):
-        w_h.Divide(w_norm)
-        for i,b in enumerate(w_h):
-            w_h[i] -= 1
-            if w_h[i] != -1: bins.append( w_h[i] )
-        w_h.SetLineColor(kBlue)
-        w_h.SetLineWidth(2)
-    for z_h in (z_up,z_dn):
-        z_h.Divide(z_norm)
-        for i,b in enumerate(z_h):
-            z_h[i] -= 1
-            if z_h[i] != -1: bins.append( z_h[i] )
-        z_h.SetLineColor(kRed)
-        z_h.SetLineWidth(2)
+    z_r_up = z_up.Clone('ratio_up'); z_r_up.Divide(z_norm)
+    z_r_dn = z_dn.Clone('ratio_dn'); z_r_dn.Divide(z_norm)
+    
+    w_r_up = w_up.Clone('ratio_up'); w_r_up.Divide(w_norm)
+    w_r_dn = w_dn.Clone('ratio_dn'); w_r_dn.Divide(w_norm)
 
-    maxY = max(bins)
-    minY = min(bins)
-    c = TCanvas(uncname,'',800,800)
+    c = TCanvas("c", "canvas",800,800);
     gStyle.SetOptStat(0);
     gStyle.SetLegendBorderSize(0);
-    c.SetLeftMargin(0.15)
-    for h in (z_up,z_dn,w_up,w_dn):
-        h.GetXaxis().SetTitle(sample.name)
-        h.GetYaxis().SetRangeUser(minY*1.2,maxY*1.2)
-        h.GetYaxis().SetTitle(uncname)
+    #c.SetLeftMargin(0.15);
+    #c.SetLogy();
+    #c.cd();
+    
+    pad1 = TPad("pad1","pad1",0.01,0.25,0.99,0.99);
+    pad1.Draw(); pad1.cd();
+    pad1.SetLogy();
+    pad1.SetFillColor(0); pad1.SetFrameBorderMode(0); pad1.SetBorderMode(0);
+    pad1.SetBottomMargin(0.);
+
+    ymax = max( h.GetMaximum() for h in (z_norm,w_norm,z_up,z_dn,w_up,w_dn) ) * pow(10,2.5)
+    ymin = ymax * pow(10,-6)
+
+    z_norm.SetLineColor(kRed)
+    for h in (z_up,z_dn): h.SetLineStyle(2); h.SetLineColor(kRed)
+
+    w_norm.SetLineColor(kBlue)
+    for h in (w_up,w_dn): h.SetLineStyle(2); h.SetLineColor(kBlue)
+    for h in (z_norm,w_norm,z_up,z_dn,w_up,w_dn):
+        h.SetTitle("")
+        h.GetYaxis().SetTitle("Events")
+        h.GetYaxis().SetRangeUser(ymin,ymax)
         h.Draw('hist same')
     
-    leg = getLegend(0.62,0.60,0.86,0.887173)
-    leg.AddEntry(z_up,'Z+jets','l')
-    leg.AddEntry(w_up,'W+jets','l')
-    leg.Draw()
+    lumi_label = '%s' % float('%.3g' % (sample.lumi/1000.)) + " fb^{-1}"
+    texLumi,texCMS = getCMSText(lumi_label,sample.version)
+    texLumi.Draw();
+    texCMS.Draw();
 
+    pt = TPaveText(0.62,0.7,0.86,0.887173,'NDC');
+    pt.AddText("Systematic Variation")
+    pt.AddText(uncname)
+    pt.Draw()
+
+    leg = getLegend(0.62,0.5,0.86,0.7)
+    leg.AddEntry(z_norm,'Z+jets','l')
+    leg.AddEntry(w_norm,'W+jets','l')
+    leg.Draw()
+    
+    c.cd();
+    pad2 = TPad("pad2","pad2",0.01,0.01,0.99,0.25);
+    pad2.Draw(); pad2.cd();
+    pad2.SetFillColor(0); pad2.SetFrameBorderMode(0); pad2.SetBorderMode(0);
+    pad2.SetTopMargin(0);
+    pad2.SetBottomMargin(0.35);
+
+    rbins = []
+    for r in (z_r_up,z_r_dn,w_r_up,w_r_dn): rbins += [ b for b in r if b != 0 ]
+
+    def avg(bins): return sum(bins)/len(bins)
+    def stdv(bins):
+        bavg = avg(bins)
+        return ( sum( (b - bavg)**2 for b in bins )/(len(bins)-1) ) ** 0.5
+    
+    rstdv = stdv(rbins)
+    rymin = 1 - 3*rstdv; rymax = 1 + 3*rstdv
+
+    for r in (z_r_up,z_r_dn): r.SetLineColor(kRed)
+    for r in (w_r_up,w_r_dn): r.SetLineColor(kBlue)
+    for r in (z_r_up,z_r_dn,w_r_up,w_r_dn):
+        RatioStyle(r,rymin,rymax)
+        r.SetMarkerStyle(1)
+        r.SetTitle("")
+        r.GetXaxis().SetTitle("");
+        r.GetXaxis().SetTickLength(0);
+        r.GetXaxis().SetLabelOffset(999);
+        r.GetYaxis().SetTitle("");
+        r.GetYaxis().SetTickLength(0);
+        r.GetYaxis().SetLabelOffset(999);
+        r.Draw('same')
+    
+    
+    nbins = z_norm.GetNbinsX();
+    xmin = z_norm.GetXaxis().GetXmin();
+    xmax = z_norm.GetXaxis().GetXmax();
+    xwmin = xmin;
+    xwmax = xmax;
+    
+    line = getRatioLine(xmin,xmax)
+    line.Draw("same");
+
+    xname = sample.name if type(sample.name) == str else None
+    xaxis = makeXaxis(xmin,xmax,rymin,510,name=xname);
+    xaxis.Draw("SAME");
+
+    yaxis = makeYaxis(rymin,rymax,xmin,6,name="syst./cent.");
+    yaxis.Draw("SAME");
+    
     outdir = out_dir % sample.version
-    outdir = "%s/SignalRegionPlots_EWK/UncertaintyPlots/"  % outdir
+    outdir = "%s/%sPlots_EWK/UncertaintyPlots/"  % (outdir,sample.region)
     if not os.path.isdir(outdir): os.mkdir(outdir)
     
     outname = "%s_%s" % (uncname,sample.varname)
