f705b14ea0c77f44d913b52cccf961e5708ec1d3
ekoenig4 <ekoenig4@wisc.edu>
2019-10-18 14:02:13 -0500

    updated

--- PlotTool/cfg_saveplot.py
+++ PlotTool/cfg_saveplot.py
@@ -3,30 +3,21 @@ config = {
     'regions':{'SignalRegion/':'8','DoubleEleCR/':'10','DoubleMuCR/':'10','SingleEleCR/':'10','SingleMuCR/':'10'},
     'Uncertainty':{
         'PFU': [ # ID is relative to the regions basic histogram number
-            'PFU_tracker',
+            # 'PFU_tracker',
             'PFU_ecal',
-            'PFU_hcal'
+            # 'PFU_hcal'
         ],
         'JES': [
             'JES'
         ],
         'QCD': [
-            "QCD_LO",
-            "QCD_K_LO",
-            "QCD_d1K_LO",
-            "QCD_d2K_LO",
-            "QCD_K_NLO",
-            "QCD_d1K_NLO",
-            "QCD_d2K_NLO",
-            "QCD_d3K_NLO",
-            "QCD_kappa_NLO_EW",
-            "QCD_kappa_NNLO_Sud",
-            "QCD_kappa_EW",
-            "QCD_d1kappa_EW",
-            "QCD_d2kappa_EW",
-            "QCD_d3kappa_EW",
-            "QCD_dK_NLO_mix",
-            "QCD_gammaind_LO"
+            "QCD_Scale",
+            "QCD_Shape",
+            "QCD_Proc",
+            "NNLO_EWK",
+            "NNLO_Miss",
+            "NNLO_Sud",
+            "QCD_EWK_Mix"
         ],
     }
 }
