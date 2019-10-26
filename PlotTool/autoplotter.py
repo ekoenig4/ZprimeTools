b13e8b028f678cad9de4052f49b02617f5eec3fe
ekoenig4 <ekoenig4@wisc.edu>
2019-10-26 13:46:07 -0500

    updating

---
+++ PlotTool/autoplotter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from Parser import PlotParser as parser
+from cfg_saveplot import config
+import os
+from plotter import plotter
+
+parser.add_argument("--nhist",help="specify histograms at basic cut level",nargs="*",default=[])
+args = parser.parse_args()
+region = os.getcwd().split('/')[-1]+'/'
+nhist = config['regions'][region]
+variables = args.argv
+variables += [ '%s_%s' % (variable,nhist) for variable in args.nhist ]
+plotter(variables)
