92ef27d113e922721787223b35e0983626665717
ekoenig4 <ekoenig4@wisc.edu>
2019-10-28 13:30:34 -0500

    updating

--- DoubleEleCR/ZprimeJetsClass.C
+++ DoubleEleCR/ZprimeJetsClass.C
@@ -208,8 +208,8 @@ ApplyKFactor(event_weight);
 	      for(int j=0; j<elelist_subleading.size(); ++j){
 		//Event must have exactly two loose electrons with opposite charge
 		if(eleCharge->at(elelist_leading[i])*eleCharge->at(elelist_subleading[j]) == -1){
-		  e1.SetPtEtaPhiE(elePt->at(elelist_leading[i]),eleSCEta->at(elelist_leading[i]),eleSCPhi->at(elelist_leading[i]),eleE->at(elelist_leading[i]));
-		  e2.SetPtEtaPhiE(elePt->at(elelist_subleading[j]),eleSCEta->at(elelist_subleading[j]),eleSCPhi->at(elelist_subleading[j]),eleE->at(elelist_subleading[j]));
+		  e1.SetPtEtaPhiE(elePt->at(elelist_leading[i]),eleEta->at(elelist_leading[i]),elePhi->at(elelist_leading[i]),eleE->at(elelist_leading[i]));
+		  e2.SetPtEtaPhiE(elePt->at(elelist_subleading[j]),eleEta->at(elelist_subleading[j]),elePhi->at(elelist_subleading[j]),eleE->at(elelist_subleading[j]));
 		  elePairSet = true;
 		  lepindex_leading = elelist_leading[i];
 		  lepindex_subleading = elelist_subleading[j];
@@ -395,11 +395,11 @@ void ZprimeJetsClass::fillHistos(int histoNumber,float event_weight){
   //CR Histograms
   if(lepindex_leading >= 0 && lepindex_subleading >= 0){
     h_leadingLeptonPt[histoNumber]->Fill(elePt->at(lepindex_leading),event_weight);
-    h_leadingLeptonEta[histoNumber]->Fill(eleSCEta->at(lepindex_leading),event_weight);
-    h_leadingLeptonPhi[histoNumber]->Fill(eleSCPhi->at(lepindex_leading),event_weight);
+    h_leadingLeptonEta[histoNumber]->Fill(eleEta->at(lepindex_leading),event_weight);
+    h_leadingLeptonPhi[histoNumber]->Fill(elePhi->at(lepindex_leading),event_weight);
     h_subleadingLeptonPt[histoNumber]->Fill(elePt->at(lepindex_subleading),event_weight);
-    h_subleadingLeptonEta[histoNumber]->Fill(eleSCEta->at(lepindex_subleading),event_weight);
-    h_subleadingLeptonPhi[histoNumber]->Fill(eleSCPhi->at(lepindex_subleading),event_weight);
+    h_subleadingLeptonEta[histoNumber]->Fill(eleEta->at(lepindex_subleading),event_weight);
+    h_subleadingLeptonPhi[histoNumber]->Fill(elePhi->at(lepindex_subleading),event_weight);
   }
   if(dilepton_pt >= 0 && dilepton_mass >= 0){
     h_recoil[histoNumber]->Fill(recoil,event_weight);
