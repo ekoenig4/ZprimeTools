59de3e45dd27d2753d3f6234c6053aaa229f1c9b
ekoenig4 <ekoenig4@wisc.edu>
2019-11-04 11:18:06 -0600

    added rest of egamma eras

--- DoubleEleCR/submitDATA.sh
+++ DoubleEleCR/submitDATA.sh
@@ -1,4 +1,4 @@
-RUN="python ${CMSSW_BASE}/src/ZprimeTools2017/CondorTools/SubmitCondor.py -y 2017 -r ZE analyze"
+RUN="python ${CMSSW_BASE}/src/ZprimeTools2017/CondorTools/SubmitCondor.py -y 2017 -r ZE -f analyze"
 
 . "${CMSSW_BASE}/src/ZprimeTools2017/plugins/data_cfg.sh"
 
@@ -9,6 +9,8 @@ echo "Do the CR Data samples"
 file="DoubleEle"
 label="Ele"
 
+ELE_A "$RUN" $file $label
 ELE_C "$RUN" $file $label
 ELE_D "$RUN" $file $label
 ELE_E "$RUN" $file $label
+ELE_F "$RUN" $file $label
--- SingleEleCR/submitDATA.sh
+++ SingleEleCR/submitDATA.sh
@@ -1,4 +1,4 @@
-RUN="python ${CMSSW_BASE}/src/ZprimeTools2017/CondorTools/SubmitCondor.py -y 2017 -r WE analyze"
+RUN="python ${CMSSW_BASE}/src/ZprimeTools2017/CondorTools/SubmitCondor.py -y 2017 -r WE -f analyze"
 
 . "${CMSSW_BASE}/src/ZprimeTools2017/plugins/data_cfg.sh"
 
@@ -9,6 +9,8 @@ echo "Do the CR Data samples"
 file="SingleEle"
 label="Ele"
 
+ELE_B "$RUN" $file $label
 ELE_C "$RUN" $file $label
 ELE_D "$RUN" $file $label
 ELE_E "$RUN" $file $label
+ELE_D "$RUN" $file $label
