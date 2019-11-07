eb454e46ccd2b17022ac9545e05ef772e6754a11
ekoenig4 <ekoenig4@wisc.edu>
2019-11-07 16:44:49 -0600

    updating

--- LumiMonitor/submit.py
+++ LumiMonitor/submit.py
@@ -6,11 +6,11 @@ repo = os.path.realpath('../')
 sys.path.append(repo)
 
 from CondorTools.SubmitDataset import submit,options
-options['year'] = '2018'
+options['year'] = '2017'
 options['region'] = 'LM'
 options['parallel'] = True
 #----Submit---#
-submit('egamma',sub='D',script='calcLumi.py',filelist=True)
+submit('egamma',sub='F',script='calcLumi.py',filelist=True)
 # submit('met',script='calcLumi.py',filelist=True)
 # submit('egamma',script='calcLumi.py',filelist=True)
 
