cfda3ca3645b579096ace1afcbe4bf3ad7e478cc
ekoenig4 <ekoenig4@wisc.edu>
2019-11-11 13:45:18 -0600

    moved year specific info to config folder and updated PlotTool to find it

--- PostAnalyzer/PlotTool/Plot.py
+++ PostAnalyzer/PlotTool/Plot.py
@@ -38,6 +38,18 @@ def GetMCxsec(filenames,xsecMap):
     return { fname:xsecMap[fname] for fname in filenames }
 def IsSignal(signal):
     return 'Mx' in signal and '_Mv' in signal
+def FindConfig():
+    wd,wf = os.path.split(__file__)
+    def updirectory(path): path += '/../'; return path
+    repo_path = os.path.realpath( updirectory(wd) )
+    def helper(path):
+        if any( directory == 'config' for directory in os.listdir(path) ): return path
+        elif os.path.realpath(path) != repo_path: return helper( updirectory(path) )
+    config_path = helper('.')
+    if config_path == None: print 'year config directory was not found in current or parent directory'; exit(1)
+    else: sys.path.append(config_path)
+
+FindConfig()
 
 class datamc(object):
 
