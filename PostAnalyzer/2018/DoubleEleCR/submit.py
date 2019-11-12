ae366538414a00cc3906e7f004a3ad093dc9f668
ekoenig4 <ekoenig4@wisc.edu>
2019-11-12 10:24:36 -0600

    updated repo name

--- PostAnalyzer/2018/DoubleEleCR/submit.py
+++ PostAnalyzer/2018/DoubleEleCR/submit.py
@@ -2,7 +2,7 @@
 import os
 import sys
 cmssw = os.getenv("CMSSW_BASE")
-repo = '%s/src/MonoZprimeJet/PostAnalyzer/' % cmssw
+repo = '%s/src/ZprimeTools/PostAnalyzer/' % cmssw
 sys.path.append(repo)
 
 from CondorTools.SubmitDataset import submit,options
