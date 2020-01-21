43e3e25c6d519f7e3a13977f5773e2959b756cbc
ekoenig4 <ekoenig4@wisc.edu>
2020-01-21 11:03:59 -0600

    added NLO handling in postAnalyzer for 2017

--- PostAnalyzer/2017/SingleEleCR/src/ZprimeClass.C
+++ PostAnalyzer/2017/SingleEleCR/src/ZprimeClass.C
@@ -44,7 +44,6 @@ void ZprimeClass::Loop(Long64_t maxEvents, int reportEvery) {
     float event_weight = 1.;
     if (!sample.isData) {
       ApplyPileup(event_weight);
-      
       if (isW_or_ZJet()) {
 	SetBoson(sample.PID);
 	ApplyKFactor(event_weight);
